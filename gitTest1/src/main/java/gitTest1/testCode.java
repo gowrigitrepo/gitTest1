d1f953e789853f31ab83abd6caad6c7654c9be99
gowri <gowri@192.168.0.38>
2022-10-04 13:32:27 -0400

    commit today

--- gitTest1/src/main/java/gitTest1/testCode.java
+++ gitTest1/src/main/java/gitTest1/testCode.java
@@ -1,13 +1,45 @@
 package gitTest1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class testCode {
 
+	static WebDriver driver;
+	static By locator;
+
+	public static void waitMethod(By locator) {
+		// Explicit wait
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
+
 	@Test
-	public static void testMaven() {
+	public static void jenkinsTest() {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.get("https://qa-tekarch.firebaseapp.com/");
+
+		locator = By.id("email_field"); // identifying the locator
+
+		waitMethod(locator);
+
+		WebElement username = driver.findElement(locator); // locating the webelement
+
+		username.sendKeys("admin123@gmail.com"); // sending the value of the web element
+
+		driver.findElement(By.id("password_field")).sendKeys("admin123");
+		driver.findElement(By.tagName("button")).click();
+
 	}
 
 }
